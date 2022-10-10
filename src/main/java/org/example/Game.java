28ced74f4e95270f18e88348693ef9d02c3de80c
Manuel Amet <emmanuelamet11@gmail.com>
2022-10-10 20:35:29 +0000

    Game class added.

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Game {
+    private int loss;
+    private int win;
+    private int push;
+
+    public Game(){
+        loss = 0;
+        win = 0;
+        push = 0;
+    }
+}
